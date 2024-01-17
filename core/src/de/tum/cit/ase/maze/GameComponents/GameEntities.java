68aaeee6c6ecca43c1e2e2fbc4d5bf9b023c4f34
Supratik Patel <superuser.patel@tum.de>
2024-01-18 00:21:27 +0100

    Trap, key and enenmy- SuperSU

---
+++ core/src/de/tum/cit/ase/maze/GameComponents/GameEntities.java
@@ -0,0 +1,38 @@
+package de.tum.cit.ase.maze.GameComponents;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class GameEntities {
+    protected Vector2 position;
+    Texture texture;
+    public boolean destroyFlAG=false;
+
+    public GameEntities(Vector2 position, Texture texture) {
+        this.position = position;
+        this.texture = texture;
+
+    }
+    public void update(){
+
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public Rectangle rectangle(){
+        return new Rectangle(position.x, position.y, texture.getWidth(), texture.getHeight());
+    }
+    public void draw(Batch batch){
+        batch.begin();
+        batch.draw(texture,position.x,position.y,texture.getWidth(),texture.getHeight(),0,0,texture.getWidth(),texture.getHeight(),false,false);
+        batch.end();
+    }
+}
---
+++ core/src/de/tum/cit/ase/maze/GameComponents/Traps.java
@@ -0,0 +1,10 @@
+package de.tum.cit.ase.maze.GameComponents;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class Traps extends GameEntities {
+    public Traps(Vector2 position, Texture texture) {
+        super(position, new Texture("trap.png"));
+    }
+}
