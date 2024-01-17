1134e1a1e80034cbcf4198d5c3fa7d4d5608cb6f
Supratik Patel <superuser.patel@tum.de>
2024-01-17 23:44:28 +0100

    test

---
+++ core/src/de/tum/cit/ase/maze/GameComponents/CellType.java
@@ -0,0 +1,28 @@
+package de.tum.cit.ase.maze.GameComponents;
+
+public enum CellType {
+    WALL,
+    ENTRY_POINT,
+    EXIT,
+    TRAP,
+    ENEMY,
+    KEY;
+
+    public static CellType fromId(int x) {
+        switch(x) {
+            case 0:
+                return WALL;
+            case 1:
+                return ENTRY_POINT;
+            case 2:
+                return EXIT;
+            case 3:
+                return TRAP;
+            case 4:
+                return ENEMY;
+            case 5:
+                return KEY;
+        }
+        return null;
+    }
+}
