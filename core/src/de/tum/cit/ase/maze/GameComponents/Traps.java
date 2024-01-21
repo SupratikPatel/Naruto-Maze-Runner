69dd4fa27f00b85f8c9d40622bc81e93b0f854b1
Supratik Patel <superuser.patel@tum.de>
2024-01-21 16:11:21 +0100

    Added Player movement- SuperSU

--- core/src/de/tum/cit/ase/maze/GameComponents/Traps.java
+++ core/src/de/tum/cit/ase/maze/GameComponents/Traps.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
 public class Traps extends GameEntities {
-    public Traps(Vector2 position, Texture texture) {
+    public Traps(Vector2 position,Texture texture) {
         super(position, new Texture("trap.png"));
     }
 }
