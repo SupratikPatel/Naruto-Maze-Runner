eaab55f0847b15167f4d97c4f59f8fe15bd6ac35
Muhammad Wahaj Khalil <wahaj.khalil@tum.de>
2024-01-29 01:29:26 +0100

    wahaj

---
+++ core/src/de/tum/cit/ase/maze/Screens/GameOverScreen.java
@@ -0,0 +1,94 @@
+package de.tum.cit.ase.maze.Screens;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class GameOverScreen implements Screen {
+
+    private final Stage stage;
+    private final MazeRunnerGame game;
+
+    public GameOverScreen(MazeRunnerGame game,int score,float time) {
+        this.game = game;
+        var camera = new OrthographicCamera();
+        camera.zoom = 1.5f; // Set camera zoom for a closer view
+
+        Viewport viewport = new ScreenViewport(camera); // Create a viewport with the camera
+        stage = new Stage(viewport, game.getSpriteBatch()); // Create a stage for UI elements
+        setupUI(score, time);
+    }
+
+    private void setupUI(int score, float time) {
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        Label gameOverLabel = new Label("GameOver!!", game.getSkin(), "title");
+        Label scoreLabel = new Label("Score: " + score, game.getSkin(), "title");
+        Label timeLabel = new Label("Time: " + String.format("%.2fs", time), game.getSkin(), "title");
+
+        gameOverLabel.setColor(Color.RED);
+        table.add(gameOverLabel).padBottom(80).row();
+        table.add(scoreLabel).padBottom(80).row();
+        table.add(timeLabel).padBottom(80).row();
+
+        TextButton goToGameButton = new TextButton("Menu", game.getSkin());
+        table.add(goToGameButton).width(300).row();
+
+        goToGameButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.goToMenu();
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear the screen
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f)); // Update the stage
+        stage.draw(); // Draw the stage
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true); // Update the stage viewport on resize
+    }
+
+    @Override
+    public void dispose() {
+        // Dispose of the stage when screen is disposed
+        stage.dispose();
+    }
+
+    @Override
+    public void show() {
+        // Set the input processor so the stage can receive input events
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    // The following methods are part of the Screen interface but are not used in this screen.
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+}
--- core/src/de/tum/cit/ase/maze/Screens/GameScreen.java
+++ core/src/de/tum/cit/ase/maze/Screens/GameScreen.java
@@ -1,85 +1,354 @@
 package de.tum.cit.ase.maze.Screens;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.*;
+import de.tum.cit.ase.maze.GameComponents.GameEntities;
+import de.tum.cit.ase.maze.Utilities.Camera;
+import de.tum.cit.ase.maze.Utilities.Manager;
+import de.tum.cit.ase.maze.GameComponents.*;
+import de.tum.cit.ase.maze.GameComponents.Enemy;
+import de.tum.cit.ase.maze.GameComponents.Key;
+import de.tum.cit.ase.maze.GameComponents.Traps;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The GameScreen class is responsible for rendering the gameplay screen.
  * It handles the game logic and rendering of the game elements.
  */
-public class GameScreen  implements Screen {
+public class GameScreen implements Screen {
 
     private final MazeRunnerGame game;
     private final OrthographicCamera camera;
     private final BitmapFont font;
+    private final Stage stage;
 
-    private float sinusInput = 0f;
+    private Viewport viewport;
 
-    /**
-     * Constructor for GameScreen. Sets up the camera and font.
-     *
-     * @param game The main game class, used to access global resources and methods.
-     */
-    public GameScreen(MazeRunnerGame game) {
+    List<GameEntities> objects;
+    Batch batch;
+    Maps map;
+    Player player;
+    Camera followCamera;
+
+    int score ;
+    float time ;
+    int heart;
+    int keyCount;
+
+    Label scoreLabel;
+    Label heartLabel;
+    Label timeLabel;
+    Label keyLabel;
+    Label bananaLabel;
+    Label missingKeyLabel;
+
+    Table pauseMenu;
+    boolean gamePause = false;
+
+    public GameScreen(MazeRunnerGame game,String mapPath) {
         this.game = game;
+        objects = new ArrayList();
+
 
         // Create and configure the camera for the game view
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false);
-        camera.zoom = 0.75f;
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+//       camera.setToOrtho(false);
+        viewport = new ScreenViewport(camera);
+        stage = new Stage(); // Create a stage for UI elements
+
+//        viewport = new FillViewport(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(),camera);
+//        ExtendViewportPermalink
 
         // Get the font from the game's skin
         font = game.getSkin().getFont("font");
+
+        batch = game.getSpriteBatch();
+        map = new Maps(mapPath,objects);
+        player = new Player(new Vector2(map.getEntryBlock().getColumn() * 16,map.getEntryBlock().getRow() * 16));
+        initInput();
+        followCamera = new Camera(camera,map);
+
+        spawnBanana();
+
+        score  = 0;
+        time = 0;
+        heart = 3;
+        keyCount = 0;
+        createUI();
+
     }
 
+    void createUI(){
+        Table table = new Table(); // Create a table for layout
+        table.setFillParent(true); // Make the table fill the stage
+        stage.addActor(table); // Add the table to the stage
+        table.center().bottom();
+
+
+        // heart
+        Table heartTable = new Table();
+        table.add(heartTable);
+//        heartTable.setBackground(new TextureRegionDrawable(new Texture("uiBox.png")));
+
+        Image hearthImg = new Image(new Texture("heart.png"));
+        heartLabel = new Label("1",game.getSkin());
+        heartLabel.setColor(Color.BLACK);
+        heartTable.add(heartLabel).pad(10);
+        heartTable.add(hearthImg);
+
+
+        // score
+        Table scoreTable = new Table();
+        table.add(scoreTable);
+//        scoreTable.setBackground(new TextureRegionDrawable(new Texture("uiBox.png")));
+
+        Label label2 = new Label("Score:",game.getSkin());
+        scoreLabel = new Label("3400",game.getSkin());
+        label2.setColor(Color.BLACK);
+        label2.setFontScale(0.6f);
+        scoreLabel.setFontScale(0.6f);
+        scoreLabel.setColor(Color.CYAN);
+        scoreTable.add(label2).pad(10);
+        scoreTable.add(scoreLabel);
+
+        // time
+        Table timeTable = new Table();
+        table.add(timeTable);
+//        timeTable.setBackground(new TextureRegionDrawable(new Texture("uiBox.png")));
+
+        Label label3 = new Label("Time:",game.getSkin());
+        timeLabel = new Label("120s",game.getSkin());
+        label3.setColor(Color.BLACK);
+        label3.setFontScale(0.6f);
+        timeLabel.setFontScale(0.6f);
+        timeLabel.setColor(Color.BLUE);
+        timeTable.add(label3).pad(10);
+        timeTable.add(timeLabel);
+
+        // key
+        Table keyTable = new Table();
+        table.add(keyTable);
+//        keyTable.setBackground(new TextureRegionDrawable(new Texture("uiBox.png")));
+
+        keyLabel = new Label("0/" + map.getKeyCount(),game.getSkin());
+        keyLabel.setFontScale(0.6f);
+        keyLabel.setColor(Color.YELLOW);
+        keyTable.add(keyLabel).pad(10);
+        keyTable.add(new Image(new Texture("key.png")));
+
+        missingKeyLabel = new Label("missing key!",game.getSkin());
+        missingKeyLabel.setVisible(false);
+        missingKeyLabel.setColor(Color.WHITE);
+        missingKeyLabel.setPosition(Gdx.graphics.getWidth()/2 - missingKeyLabel.getWidth()/2,Gdx.graphics.getHeight()/2 - missingKeyLabel.getHeight()/2);
+        stage.addActor(missingKeyLabel);
+
+
+        pauseMenu = new Table();
+        pauseMenu.setFillParent(true);
+
+        pauseMenu.add(new Label("ESC: Continue!",game.getSkin())).padBottom(20).row();
+
+
+        TextButton loadBtn = new TextButton("Load", game.getSkin());
+        pauseMenu.add(loadBtn).width(200);
+        loadBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.loadGame(); // Change to the game screen when button is pressed
+            }
+        });
+        TextButton gotoMenuBtn = new TextButton("Menu", game.getSkin());
+        pauseMenu.add(gotoMenuBtn).width(200).padLeft(50).row();
+        gotoMenuBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.goToMenu(); // Change to the game screen when button is pressed
+            }
+        });
+
+        pauseMenu.setVisible(false);
+
+        stage.addActor(pauseMenu);
+        Manager.getInstance().soundsManager.playGameMusic();
+    }
+
+    void initInput(){
+        InputMultiplexer mixInput = new InputMultiplexer();
+        mixInput.addProcessor(stage);
+        mixInput.addProcessor(new InputAdapter(){
+            @Override
+            public boolean keyDown(int keycode) {
+                player.onKeyDown(keycode);
+                return super.keyDown(keycode);
+            }
+
+            @Override
+            public boolean keyUp(int keycode) {
+                player.onKeyUp(keycode);
+                return super.keyUp(keycode);
+            }
+        });
+        Gdx.input.setInputProcessor(mixInput);
+    }
 
     // Screen interface methods with necessary functionality
     @Override
     public void render(float delta) {
-        // Check for escape key press to go back to the menu
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
-            game.goToMenu();
+//            game.goToVictoryScreen();
+            gamePause = !gamePause;
+            pauseMenu.setVisible(gamePause);
+        }
+        ScreenUtils.clear(0, 0, 0, 1);
+
+//        camera.position.x += (viewport.getScreenX() + viewport.getScreenWidth()/4 + delta - camera.position.x) * 1 * delta;
+        camera.update();
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        if(!gamePause){
+            followCamera.follow(player.getPosition());
+
+            // update time,score,heart
+            time += delta;
+            timeLabel.setText(String.format("%.2f",time) + "s");
+            scoreLabel.setText(score);
+            heartLabel.setText(heart);
+            keyLabel.setText(keyCount + "/" + map.getKeyCount());
+
+            List<Entity> addedObjects = new ArrayList<>();
+            player.update(map);
+            player.shoot(addedObjects);
+
+
+            for(Entity obj : objects){
+                obj.update();
+            }
+            objects.addAll(addedObjects);
+
+            missingKeyLabel.setVisible(false);
+            for(int row = 0; row < map.getNum_Of_Rows(); row++) {
+                for (int col = 0; col < map.getNum_Of_Column(); col++) {
+                    Block block = map.getCell(row,col);
+                    if(block.blockType == BlockType.EXIT && block.getRect().collide(player.getRect())){
+                        if(keyCount == map.getKeyCount()){
+                            score += 100;
+                            game.goToVictoryScreen(score,time);
+                            return;
+                        }
+                        else {
+                            missingKeyLabel.setVisible(true);
+                        }
+                    }
+                }
+            }
+
+            // collision player with other objects
+            for(Entity obj : objects){
+                if(obj instanceof Ghost2){
+                    ((Ghost2)obj).setPlayer(player);
+                }
+                if(obj instanceof Enemy && obj.getRect().collide(player.getRect())){
+                    playerDie();
+                }
+                else if(obj instanceof Key && obj.getRect().collide(player.getRect())){
+                    obj.destroyFlag = true;
+                    keyCount++;
+                    score += 50;
+                    Manager.getInstance().soundsManager.play("item",1.0f);
+                }
+                else if(obj instanceof Trap && obj.getRect().collide(player.getRect())){
+                    playerDie();
+                }
+            }
+
+            // collision between objects
+            for(Entity obj : objects){
+                for(Entity otherObj: objects){
+                    if(obj == otherObj){
+                        continue;
+                    }
+                    if(obj instanceof PlayerProjectile && (otherObj instanceof Enemy || otherObj instanceof Trap)){
+                        if(obj.getRect().collide(otherObj.getRect())){
+                            score += 5;
+                            obj.destroyFlag = true;
+                            otherObj.destroyFlag = true;
+                        }
+                    }
+
+                }
+            }
+
+            for(int i = 0; i < objects.size();i++){
+                if(objects.get(i).destroyFlag){
+                    objects.remove(i);
+                }
+            }
         }
 
-        ScreenUtils.clear(0, 0, 0, 1); // Clear the screen
+        //draw
+        map.draw(batch,player);
 
-        camera.update(); // Update the camera
+        for(Entity obj : objects){
+            if(Vector2.dst(player.getPosition().x,player.getPosition().y,obj.getPosition().x,obj.getPosition().y) > 2000){ // fog of war
+                continue;
+            }
+            obj.draw(batch);
+        }
 
-        // Move text in a circular path to have an example of a moving object
-        sinusInput += delta;
-        float textX = (float) (camera.position.x + Math.sin(sinusInput) * 100);
-        float textY = (float) (camera.position.y + Math.cos(sinusInput) * 100);
+        player.draw(batch);
 
-        // Set up and begin drawing with the sprite batch
-        game.getSpriteBatch().setProjectionMatrix(camera.combined);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f)); // Update the stage
+        stage.draw(); // Draw the stage
 
-        game.getSpriteBatch().begin(); // Important to call this before drawing anything
 
-        // Render the text
-        font.draw(game.getSpriteBatch(), "Press ESC to go to menu", textX, textY);
 
-        // Draw the character next to the text :) / We can reuse sinusInput here
-        game.getSpriteBatch().draw(
-                game.getCharacterDownAnimation().getKeyFrame(sinusInput, true),
-                textX - 96,
-                textY - 64,
-                64,
-                128
-        );
-
-        game.getSpriteBatch().end(); // Important to call this after drawing everything
+        viewport.apply(false);
     }
+
+    void playerDie(){
+        heart--;
+        Block entryBlock = map.getEntryCell();
+        player.setPosition(new Vector2(entryBlock.col * 16, entryBlock.row * 16));
+        Manager.getInstance().soundsManager.play("death",1.0f);
+        if(heart <= 0){
+            game.goToGameOverScreen(score,time);
+        }
+    }
+
+    void spawnBanana(){
+        Block emptyBlock = map.getRandomEmptyCell();
+        while (Vector2.dst(player.getPosition().x,player.getPosition().y, emptyBlock.col*16, emptyBlock.row * 16) > 200){
+            emptyBlock = map.getRandomEmptyCell();
+        }
+    }
+
 
     @Override
     public void resize(int width, int height) {
         camera.setToOrtho(false);
+        viewport.update(width,height,false);
+
     }
 
     @Override
     public void pause() {
+
     }
 
     @Override
