c407f3ea1bc6f88bb05eef32ac5c93e1c1646552
Supratik Patel <superuser.patel@tum.de>
2024-01-29 16:40:23 +0100

    Fixed Exit button- SuperSU

--- core/src/de/tum/cit/ase/maze/Screens/MenuScreen.java
+++ core/src/de/tum/cit/ase/maze/Screens/MenuScreen.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import de.tum.cit.ase.maze.Utilities.Manager;
@@ -44,7 +46,15 @@ public class MenuScreen implements Screen {
 
         TextButton playButton = new TextButton("Play", game.getSkin());
         TextButton loadGameButton = new TextButton("Load", game.getSkin());
-        TextButton exitButton = new TextButton("Quit", game.getSkin());
+        TextButton exitButton = new TextButton("Exit", game.getSkin());
+
+        Gdx.input.setInputProcessor(stage);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
 
         table.add(playButton).width(300);
         table.add(loadGameButton).width(300);
